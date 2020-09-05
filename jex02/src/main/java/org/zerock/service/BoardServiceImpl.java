d8910cf76c0fc92d789655a65475f16a85e4cc03
parksil <parksil00@naver.com>
2020-09-05 22:25:41 +0900

    20200905

--- jex02/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ jex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -7,9 +7,10 @@ import org.zerock.domain.BoardVO;
 import org.zerock.mapper.BoardMapper;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+@Log4j
 @Service
 @AllArgsConstructor
 public class BoardServiceImpl implements BoardService {
